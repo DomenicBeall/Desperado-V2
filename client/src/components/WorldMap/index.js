5f90ab4196e91aec28b42d4ff75f83ae3adeebf1
Domenic Beall <domenicbeall2@gmail.com>
2021-05-19 00:04:47 +1000

    Added clustering to world map

--- client/src/components/WorldMap/index.js
+++ client/src/components/WorldMap/index.js
@@ -2,17 +2,68 @@ import { Component } from 'react';
 import { AuthContext } from '../../context/auth';
 import GoogleMapReact from 'google-map-react';
 import Axios from 'axios';
+import supercluster from 'points-cluster';
 
 import "./map.css";
 import iconPawn from '../../assets/icon-pawn.svg';
+import iconPawnHollow from '../../assets/icon-pawn-hollow.svg';
+import iconCluster from '../../assets/icon-cluster.svg';
 
 class WorldMap extends Component {
 
   constructor(props) {
     super(props);
 
-    this.state = {games: [], location: { lat: -37.8136, lng: 144.9631 }};
+    this.state = {
+      games: [], 
+      location: { lat: 60.814305, lng: 47.051773 },
+      clusters: []
+    };
   }
+
+  getClusters = props => {
+    this.state.games.forEach(element => {
+      element.lat = element.location.lat;
+      element.lng = element.location.lng;
+    });
+
+    const clusters = supercluster(this.state.games, {
+      minZoom: 0,
+      maxZoom: 16,
+      radius: 60
+    });
+
+    return clusters(this.state.mapOptions);
+  };
+
+  createClusters = props => {
+    this.setState({
+      clusters: this.state.mapOptions.bounds
+        ? this.getClusters(props).map(({ wx, wy, numPoints, points }) => ({
+            lat: wy,
+            lng: wx,
+            numPoints,
+            id: `${numPoints}_${points[0].id}`,
+            points,
+          }))
+        : [],
+    });
+  };
+
+  handleMapChange = ({ center, zoom, bounds }) => {
+    this.setState(
+      {
+        mapOptions: {
+          center,
+          zoom,
+          bounds,
+        },
+      },
+      () => {
+        this.createClusters(this.props);
+      }
+    );
+  };
 
   componentDidMount() {
     if (navigator.geolocation) {
@@ -46,18 +97,25 @@ class WorldMap extends Component {
           bootstrapURLKeys={{ key: "AIzaSyALoz7rDY5iHKbGa9gWh_0EtaITjIXAQzc" }}
           center={(this.state.location)}
           defaultZoom={11}
+          onChange={this.handleMapChange}
         >
 
-        {
-          (this.state.games.length > 0)
-          ?
-            this.state.games.map((element, index) => {
-              return <img src={iconPawn} alt="Pawn icon" className="map-icon" key={index} lat={element.location.lat} lng={element.location.lng}></img>
-            })
-          :
-          <></>
-        }
+        {this.state.clusters.map((item, index) => {
+            if (item.numPoints === 1) {
+              return (
+                <div className="map-icon-parent" key={index} lat={item.points[0].lat} lng={item.points[0].lng}>
+                  <img src={iconPawn} alt="Pawn icon" className="map-icon"></img>
+                </div>
+              );
+            }
 
+            return (
+              <div className="cluster-parent-icon" key={index} lat={item.points[0].lat} lng={item.points[0].lng}>
+                <img src={iconCluster} alt="Pawn icon" className="cluster-icon" ></img>
+                <p className="cluster-num">{item.numPoints}</p>
+              </div>
+            );
+          })}
         </GoogleMapReact>
       </div>
     );
