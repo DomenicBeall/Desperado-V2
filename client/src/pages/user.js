b51e4ee45ab933212387bf8a6aecf806bcf4ef8b
Domenic Beall <domenicbeall2@gmail.com>
2021-06-30 12:03:22 +1000

    User profile page now renders based on the user id passed in the url

--- client/src/pages/user.js
+++ client/src/pages/user.js
@@ -1,30 +1,50 @@
 import { Component } from 'react';
-import { AuthContext } from '../context/auth';
+import axios from 'axios';
 
 import Header from '../components/Header';
 import UserDetails from '../components/UserDetails';
 import UserGames from '../components/UserGames';
 
 class User extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      user: null
+    };
+  }
+
+  componentDidMount() {
+    axios({
+      method: 'get',
+      url: '/api/user/' + this.props.match.params.id
+    })
+    .then((response) => {
+      this.setState({ user: response.data });
+    });
+  }
+
   render() {
     return (
       <div style={{ height: "100vh" }}>
         <Header/>
-        <div style={{ display: 'flex', justifyContent: "space-between", height: "85vh", padding: "1rem"}}>
-          <div style={{ width: "49.5%", height: "100%"}}>
-            <UserDetails user={this.context.user}></UserDetails>
-            <UserGames user={this.context.user} ></UserGames>
-          </div>
-          <div style={{ width: "50%", height: "100%", backgroundColor: "white" }}>
-            ass
-          </div>
-        </div>
+        {
+          this.state.user ?
+            <div style={{ display: 'flex', justifyContent: "space-between", height: "85vh", padding: "1rem"}}>
+              <div style={{ width: "49.5%", height: "100%"}}>
+                <UserDetails user={this.state.user}></UserDetails>
+                <UserGames user={this.state.user} ></UserGames>
+              </div>
+              <div style={{ width: "50%", height: "100%", backgroundColor: "white" }}>
+                Not sure what I'll do with this space yet
+              </div>
+            </div>
+          :
+          <div>Loading...</div>
+        }
       </div>
     );
   }
 }
-
-User.contextType = AuthContext;
-
 
 export default User;
